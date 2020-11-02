e60bac08f0ac399016be52578d1e9ca05c9be18a
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-11-02 17:37:15 -0300

    Just a curiosity using the ModelAndView object

--- src/main/java/br/com/docker/monitormanagercontainers/configs/WebConfig.java
+++ src/main/java/br/com/docker/monitormanagercontainers/configs/WebConfig.java
@@ -3,12 +3,9 @@ package br.com.docker.monitormanagercontainers.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig  {
 
     @Bean
     public ResourceBundleMessageSource messageSource(){
--- src/main/java/br/com/docker/monitormanagercontainers/controllers/IndexController.java
+++ src/main/java/br/com/docker/monitormanagercontainers/controllers/IndexController.java
@@ -3,13 +3,16 @@ package br.com.docker.monitormanagercontainers.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
 
     @GetMapping
-    public String index(){
-        return "index";
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("idade","23");
+        return modelAndView;
     }
 }
