a2d1de7d1bc88c22539ea56d5d8e92384661f662
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-11-02 12:40:08 -0300

    Init project

---
+++ src/main/java/br/com/docker/monitormanagercontainers/MonitorManagerContainersApplication.java
@@ -0,0 +1,13 @@
+package br.com.docker.monitormanagercontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorManagerContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorManagerContainersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/docker/monitormanagercontainers/MonitorManagerContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.docker.monitormanagercontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorManagerContainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
