5a27d22ad6299b2e7332c90e986b289206e65f75
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2020-11-03 21:34:24 -0300

    Create container

--- src/main/java/br/com/docker/monitormanagercontainers/clients/dtos/CreateContainerDTO.java
+++ src/main/java/br/com/docker/monitormanagercontainers/clients/dtos/CreateContainerDTO.java
@@ -1,5 +1,6 @@
 package br.com.docker.monitormanagercontainers.clients.dtos;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateContainerDTO {
+    @JsonAlias({"image","imageId"})
     private String image;
 }
